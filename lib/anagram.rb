7b93d870dc04e0b6eb0cf3afee0ae57bd67c9718
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-04 16:52:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,27 +1,2 @@
-# Your code goes here!
 class Anagram
-attr_accessor :word
-
-def intialize
-  @word = word
-end
-
-def match(n)
-  words_received = []
-  word.each_line do |line|
-    if (line.length - 1) == n.length
-      words_received << line.gsub("\n","")
-    end
-
-    confirmed_match = []
-    words_received.each do |word|
-      if word.chars.sort == n.chars.sort
-        confirmed_match << word
-      end
-end
-confirmed_match
-
-end
-
-
-end
+end # close class
