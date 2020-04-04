808887979dbb0007e61fd67b007d17406f3c01a1
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-04 16:58:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
   end
 
   def match(array) #iterate over the array of words this method takes as an argument
-    new_array = %w(array)
+    array.uniq.length == array.length
   end
 
 end # close class
