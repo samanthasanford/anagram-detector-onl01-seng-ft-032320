4fcc84bf0574f15037d191fc94b0164968bae5c8
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-04 17:03:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,12 @@
 class Anagram
   attr_accessor :words
 
-  def initialize(words)
-    @words = words
-  end
+def initialize(words)
+  @words = words
+end
 
-  def match(array) #iterate over the array of words this method takes as an argument
-    array.uniq.length == array.length
-  end
+def match(array)
+  array = (%w(@words))
+end
 
 end # close class
