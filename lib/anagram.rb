906d67b237efa32d3d688afd7242b37364112d71
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-04 16:37:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+def intialize(word)
+  @word = word
+end
+
+def match(words)
+  words.select do |i|
+    (@word.split("").sort) == (i.split("").sort)
+  end
+end
+
+end
