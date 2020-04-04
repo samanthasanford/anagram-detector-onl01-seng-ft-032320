2fc2114d17c1b8cadfafaf2137e68c6ca6ed74f8
Samantha Sanford <samanthawebdev@gmail.com>
2020-04-04 16:55:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,2 +1,12 @@
 class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(array) #iterate over the array of words this method takes as an argument
+    array.split("").sort == array.split("").sort
+  end
+
 end # close class
